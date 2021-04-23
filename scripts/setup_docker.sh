5b6378619c06337976d722ebb9f71210e2b17f11
Naïm Gómez <33960177+NgomezKS@users.noreply.github.com>
2021-04-23 18:39:42 +0200

    Update setup_docker.sh

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -1,5 +1,5 @@
 
-apt update
+sudo apt update
 sudo apt-get --force-yes --yes install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 
 sudo apt-get --force-yes --yes install \
@@ -9,13 +9,13 @@ sudo apt-get --force-yes --yes install \
     gnupg \
     lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo \
+sudo echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
