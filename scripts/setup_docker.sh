054699157223dd770e3d6f78fb1484ced3b6e1a6
Naïm Gómez <33960177+NgomezKS@users.noreply.github.com>
2021-04-23 18:54:07 +0200

    Update setup_docker.sh

--- scripts/setup_docker.sh
+++ scripts/setup_docker.sh
@@ -20,4 +20,5 @@ sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
 sudo apt-get --force-yes --yes update
-sudo apt-get --force-yes --yes install docker-ce docker-ce-cli containerd.io
+sudo apt-get --force-yes --yes install docker-ce docker-ce-cli containerd.io docker-compose
+
